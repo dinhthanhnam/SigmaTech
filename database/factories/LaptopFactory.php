4aa167faedebdf32483705341a8495a4a9d4393a
Nam <thanhnamak@gmail.com>
2024-12-15 22:53:27 +0700

    abc

--- database/factories/LaptopFactory.php
+++ database/factories/LaptopFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Laptop;
+use Attribute;
+use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Laptop>
  */
@@ -18,8 +19,25 @@ class LaptopFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'category_id' => fake()->randomElement([1, 0]),
+            'category_id' => fake()->randomElement([1, 2]),
         ];
     }
+    // public function configure()
+    // {
+    //     return $this->afterCreating(function (Laptop $laptop) {
+    //         $this->fakeLaptopAttributes($laptop);
+    //     });
+    // }
 
+    // protected function fakeLaptopAttributes(Laptop $laptop)
+    // {
+    //     // Custom logic cho attributes
+    //     $attributes = Attribute::query()->where('id', [1, 2, 3, 4, 5, 6, 7, 8, 9])->get();
+
+    //     foreach ($attributes as $attribute) {
+    //         $laptop->attributes()->attach($attribute->id, [
+    //             'value' => $this->faker->word(),
+    //         ]);
+    //     }
+    // }
 }
